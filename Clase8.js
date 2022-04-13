401e809d03ebf6940c50298f10fdaa5f8e9ea222
Carnet <juanchifpedraza@hotmail.com>
2022-04-13 16:40:46 -0300

    Desafio clase 8 terminado (casi)

--- Clase8.js
+++ Clase8.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const fs = require("fs");
-const { isTypedArray } = require('util/types');
 const { Router } = express;
 const app = express();
 const productos = Router();
 const PORT = 8080;
 
+//express
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/productos', productos);
 
@@ -18,105 +18,102 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 })
 
+// clase con los metodos
 class Contenedor {
-  constructor (archivo) {
-      this.archivo = archivo;
+  constructor(txt) {
+    this.archivo = txt;
   }
-  async save(title, price) {
-      try {
-          const data = await fs.promises.readFile(this.archivo, 'utf-8');
-          const json = JSON.parse(data.toString("utf-8"));
-          const obj = {title: title, price: price, id: json.length};
-          json.push(obj);
-          console.log(`El id del producto es ${data.length}`);
-          await fs.promises.writeFile(this.archivo, JSON.stringify(json, null, "\t"));
-      }
-      catch(err) {
-          console.log('Hubo un error');
-      }
-  }
-  async getById(id) {
-      try {
-        const data = await fs.promises.readFile(this.archivo, 'utf-8');
-        const json = JSON.parse(data.toString("utf-8"));
-        const obj = json.find(e => e.id === id);
-        return obj;
-      }
-      catch(err) {
-          console.log('Hubo un error');
-      }
-  }
-  async getAll() {
-      try {
-          const data = await fs.promises.readFile(this.archivo, 'utf-8');
-          return data;
-      }
-      catch(err) {
-          console.log('Hubo un error');
-      }
-  }
-  async deleteById(id) {
-      try {
-          const data = await fs.promises.readFile(this.archivo, 'utf-8');
-          const json = JSON.parse(data.toString("utf-8"));
-          const indexObj = json.findIndex(e => e.id === id);
-          json.splice(indexObj, 1);
-          await fs.promises.writeFile(this.archivo, JSON.stringify(json, null, "\t"));
-      }
-      catch(err) {
-          console.log('Hubo un error');
-      }
-  }
-}
-// GET '/api/productos' -> devuelve todos los productos.
-// GET '/api/productos/:id' -> devuelve un producto según su id.
-// POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado.
-// PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
-// DELETE '/api/productos/:id' -> elimina un producto según su id.
-
-const items = new Contenedor('./public/items.txt');
-
-productos.get('/', async (req, res) => {
-  console.log('productos ok');
-  const listaProd = await items.getAll();
-  res.send(listaProd);
-})
-
-productos.get('/:id', async (req, res) => {
-  console.log('producto individual ok');
-  var id = parseInt(req.params.id, 10);
-  const producto = await items.getById(id);
-  if (producto === undefined) {
-    res.json({ error : 'producto no encontrado' });
-  } else {
-      res.json(producto);}
-})
-
-productos.post('/', async (req, res) => {
-    console.log('carga de productos ok');
-    let title = req.query.title;
-    let price = req.query.price;
-    items.save(title, price);
-    res.json({
+  // guardar nuevo objeto
+  async save(req, res) {
+    try {
+      const data = await fs.promises.readFile('items.txt', 'utf-8');
+      const json = JSON.parse(data.toString("utf-8"));
+      let title = req.query.title;
+      let price = req.query.price;
+      const obj = { title: title, price: price, id: data.length };
+      json.push(obj);
+      console.log(`El id del producto es ${data.length}`);
+      await fs.promises.writeFile('items.txt', JSON.stringify(json, null, "\t"));
+      return res.json({
         success: true,
         message: "Carga exitosa"
-    });
-  })
-  
-// productos.put('/', async (req, res) => {
-//     console.log('productos ok');
-//     const listaProd = await items.getAll();
-//     res.send(listaProd);
-//     })
+      });
+    }
+    catch (err) {
+      console.log(`Hubo un error = ${err} `);
+    }
+  }
+  // obtener un objeto segun su id
+  async getById(req, res) {
+    try {
+      const data = await fs.promises.readFile('items.txt', 'utf-8');
+      const json = JSON.parse(data.toString("utf-8"));
+      const obj = json.find(e => e.id === parseInt(req.params.id, 10));
+      return res.json(obj ? obj : { error: 'producto no encontrado' });
+    }
+    catch (err) {
+      console.log(`Hubo un error = ${err} `);
+    }
+  }
+  // mostrar todos los productos
+  async getAll(req, res) {
+    try {
+      const data = await fs.promises.readFile('items.txt', 'utf-8');
+      console.log(this.archivo);
+      return res.send(data);
+    }
+    catch (err) {
+      console.log(`Hubo un error = ${err} `);
+    }
+  }
+  // borrar un producto por id
+  async deleteById(req, res) {
+    try {
+      const data = await fs.promises.readFile('items.txt', 'utf-8');
+      const json = JSON.parse(data.toString("utf-8"));
+      let id = parseInt(req.params.id, 10);
+      if (id <= data.length) {
+        const indexObj = json.findIndex(e => e.id === id);
+        json.splice(indexObj, 1);
+        await fs.promises.writeFile('items.txt', JSON.stringify(json, null, "\t"));
+        return res.json({ respuesta: 'Producto Eliminado' });
+      } else {
+        return res.json({ error: 'Producto no encontrado' });
+      }
+    }
+    catch (err) {
+      console.log(`Hubo un error = ${err} `);
+    }
+  }
+  // actualizar un objeto por su id
+  async updateById(req, res) {
+    try {
+      const data = await fs.promises.readFile('items.txt', 'utf-8');
+      const json = JSON.parse(data.toString("utf-8"));
+      let id = parseInt(req.params.id, 10);
+      const indexObj = json.findIndex(e => e.id === id);
+      let title = req.query.title;
+      let price = req.query.price;
+      json[indexObj] = { title: title, price: price, id: id };
+      await fs.promises.writeFile('items.txt', JSON.stringify(json, null, "\t"));
+      return res.json({ producto: 'Producto actualizado correctamente' });
+    }
+    catch (err) {
+      console.log(`Hubo un error = ${err} `);
+    }
+  }
+}
 
-app.delete('/:id', async (req, res) => {
-    const id = req.params.id;
-    await items.deleteById(id);
-    res.send('objeto elimina2');
-});
+const items = new Contenedor('items.txt');
+
+productos.get('/', items.getAll);
+productos.get('/:id', items.getById);
+productos.post('/', items.save);
+productos.delete('/:id', items.deleteById);
+productos.put('/:id', items.updateById);
 
 app.listen(PORT, err => {
-  if(err) throw new Error(`error en el sv ${err}`)
+  if (err) throw new Error(`error en el sv ${err}`)
   console.log(`el sv escuchar en ${PORT}`);
 })
 
