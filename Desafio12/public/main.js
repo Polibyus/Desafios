df1fd6e8049fc104e15303bd95e89ac5302091b7
Carnet <juanchifpedraza@hotmail.com>
2022-04-23 12:46:40 -0300

    Ya casi terminado desafio 12

--- Desafio12/public/main.js
+++ Desafio12/public/main.js
@@ -1,51 +1,61 @@
-const socket = io.connect();
-// mensajeria
-function render(data) {
-    const html = data.map((elem, index) => {
-        return(`<div>
-            <strong>${elem.author}</strong>:
-            <em>${elem.text}</em> </div>`)
-    }).join(" ");
-    document.getElementById('messages').innerHTML = html;
-}
+    const socket = io.connect();
 
-function addMessage(e) {
-    const mensaje = {
-        author: document.getElementById('username').value,
-        text: document.getElementById('texto').value
-    };
-    socket.emit('new-message', mensaje);
-    return false;
-}
-
-socket.on('messages', function(data) { render(data); });
-
-// agregar items
-
-function renderItem(data) {
-    if (data.length !== 0) {
-    const html = data.forEach(p => {
-        return(`
-            <td>${p.title}
-            td=${p.price}
-            td=${p.id}
-        `)})
-    document.getElementById('table').innerHTML = html;
-    } else {
-        document.getElementById('table').innerHTML = 'no hay productos';
+    // agregar items
+    
+    function addItem(i) {
+        const item = {
+            title: document.getElementById('title').value,
+            price: document.getElementById('price').value,
+            pic: document.getElementById('pic').value
+        };
+        socket.emit('new-item', item);
+        return false;
     }
-}
+    
+    function renderItem(data) {
+        const html = data.map((p) => {
+            return(`
+            <tr>
+                <td>${p.id}</td>
+                <td>${p.title}</td>
+                <td>${p.price}</td>
+                <td>
+                <img src=${p.pic} width="50px" height="50px">
+                </td>
+            </tr>`)
+        }).join(' ');
+        document.getElementById('products').innerHTML = html;
+    }
+    
+    socket.on('products', (data) => { renderItem(data); });
 
-function addItem(i) {
-    const item = {
-        title: document.getElementById('title').value,
-        price: document.getElementById('price').value,
-        pic: document.getElementById('pic').value
-    };
-    socket.emit('new-item', item);
-    return false;
-}
+    // mensajeria
+    function render(data) {
+        const html = data.map((e) => {
+            return(`<ul class="chat">
+            <li class="left clearfix">
+            <div class="chat-body clearfix">
+                <div class="header">
+                    <strong class="primary-font">${e.author}</strong> <small class="pull-right text-muted">
+                        <span class="glyphicon glyphicon-time"></span>12 mins ago</small>
+                </div>
+                <p>${e.text}</p>
+                </div>
+            </li>
+            </ul>`)
+        }).join(" ");
+        document.getElementById('messages').innerHTML = html;
+    }
 
-socket.on('productos', function(data) { renderItem(data); });
+    function addMessage(e) {
+        const mensaje = {
+            author: document.getElementById('username').value,
+            text: document.getElementById('texto').value
+        };
+        socket.emit('new-message', mensaje);
+        return false;
+    }
+
+    socket.on('messages', function(data) { render(data); });
 
 
--- Desafio12/server.js
+++ Desafio12/server.js
@@ -18,49 +18,36 @@ app.set('view engine', 'pug');
 //public
 app.use(express.static('public'));
 //rutas con express
-app.use('/productos', productos);
+app.use('/products', productos);
 
-// clase con los metodos
-class Contenedor {
-  constructor(txt) {
-    this.archivo = txt;
-  }
-  // guardar nuevo objeto
-  async save(req, res) {
-    try {
-      const data = await fs.promises.readFile('items.txt', 'utf-8');
-      const json = JSON.parse(data.toString("utf-8"));
-      let title = req.body.title;
-      let price = parseInt(req.body.price, 10);
-      let pic = req.body.pic;
-      const obj = { title: title, price: price, pic: pic, id: data.length };
-      json.push(obj);
-      await fs.promises.writeFile('items.txt', JSON.stringify(json, null, "\t"));
-      return res.render('index.pug', {
-        success: true,
-        message: `El id del producto es ${data.length}`
-      });
-    }
-    catch (err) {
-      console.log(`Hubo un error = ${err} `);
-    }
-  }
-  // mostrar todos los productos
-  async getAll(req, res) {
-    try {
-      const data = await fs.promises.readFile('items.txt', 'utf-8');
-      return res.render('index.pug', { products: JSON.parse(data.toString("utf-8")) });
-    }
-    catch (err) {
-      console.log(`Hubo un error = ${err} `);
-    }
-  }
-}
-
-const items = new Contenedor('items.txt');
-
-productos.get('/', items.getAll);
-productos.post('/', items.save);
+const products = [
+  {
+		"title": "adsf",
+		"price": 546,
+    "pic":'https://cdn3.iconfinder.com/data/icons/fantasy-and-role-play-game-adventure-quest/512/Monster-256.png',
+		"id": 2
+	},
+	{
+		"title": "Hola",
+		"price": 54,
+    "pic":'https://cdn3.iconfinder.com/data/icons/fantasy-and-role-play-game-adventure-quest/512/Fairy-256.png',
+		"id": 60
+	},
+	{
+		"title": "a ver",
+		"price": 23,
+    "pic":'https://cdn3.iconfinder.com/data/icons/fantasy-and-role-play-game-adventure-quest/512/Unicorn-256.png',
+		"id": 106
+	},
+	{
+		"title": "a ver",
+		"price": 23,
+    "pic":'https://cdn3.iconfinder.com/data/icons/fantasy-and-role-play-game-adventure-quest/512/Sword-256.png',
+		"id": 160
+	}
+]
+// productos.get('/', items.getAll);
+// productos.post('/', items.save);
 
 // chat en vivo
 
@@ -72,16 +59,65 @@ const messages = [
 
 io.on('connection',socket => {
   socket.emit('messages', messages);
-  socket.emit('productos', messages);
-  socket.on('new-message',data => {
-      messages.push(data);
-      io.sockets.emit('messages', messages);
+  socket.emit('products', products);
+  socket.on('new-message', (data) => {
+    messages.push(data);
+    io.sockets.emit('messages', messages);
   });
+  socket.on('new-item', (data) => {
+    products.push(data);
+    io.sockets.emit('products', products);
+  })
 });
 
+productos.get('/',(req,res)=>{
+
+  res.render('index.pug')
+
+})
 
 httpServer.listen(PORT, err => {
   if (err) throw new Error(`error en el sv ${err}`)
-  console.log(`el sv escuchar en ${PORT} en http://localhost:8080`);
+  console.log(`el sv escuchar en ${PORT} en http://localhost:8080/products`);
 })
+
+
+
+// // clase con los metodos
+// class Contenedor {
+//   constructor(txt) {
+//     this.archivo = txt;
+//   }
+//   // guardar nuevo objeto
+//   async save(req, res) {
+//     try {
+//       const data = await fs.promises.readFile('items.txt', 'utf-8');
+//       const json = JSON.parse(data.toString("utf-8"));
+//       let title = req.body.title;
+//       let price = parseInt(req.body.price, 10);
+//       let pic = req.body.pic;
+//       const obj = { title: title, price: price, pic: pic, id: data.length };
+//       json.push(obj);
+//       await fs.promises.writeFile('items.txt', JSON.stringify(json, null, "\t"));
+//       return res.render('index.pug', {
+//         success: true,
+//         message: `El id del producto es ${data.length}`
+//       });
+//     }
+//     catch (err) {
+//       console.log(`Hubo un error = ${err} `);
+//     }
+//   }
+//   // mostrar todos los productos
+//   async getAll(req, res) {
+//     try {
+//       const data = await fs.promises.readFile('items.txt', 'utf-8');
+//       return res.render('index.pug', { products: JSON.parse(data.toString("utf-8")) });
+//     }
+//     catch (err) {
+//       console.log(`Hubo un error = ${err} `);
+//     }
+//   }
+// }
+
 
